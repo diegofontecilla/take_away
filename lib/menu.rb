e71e37e51ddb4ef507d0eff1bfb64df6e9a63007
diego fontecilla <mail@diegofontecilla.com>
2018-08-09 12:13:25 +0100

    #see_menu print a list of dishes with prices

--- lib/menu.rb
+++ lib/menu.rb
@@ -2,7 +2,14 @@ class Menu
 
   def initialize
     @menu = [
-      {dish: 'spaghetti alle vongole', price: '12.50'}
+      {dish: 'spaghetti alle vongole', price: '12.50'},
+      {dish: 'spaghetti alla carbonara', price: '14.40'},
+      {dish: 'parmigiana', price: '11.30'},
+      {dish: 'lasagna al forno', price: '14.60'},
+      {dish: 'ragu alla bolognese', price: '12.00'},
+      {dish: 'risotto allo zafferano', price: '15.20'},
+      {dish: 'gnocchi alla sorrentina', price: '9.50'},
+      {dish: 'zucchine ripiene alla ligure', price: '14.70'}
     ]
   end
 
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -3,10 +3,18 @@ require 'menu'
 describe Menu do
 
   context '#see_menu' do
-    it 'print a dish with it\'s price' do
+    # it 'print a dish with it\'s price' do
+    #   menu = Menu.new
+    #   expect { menu.see_menu }.to output("spaghetti alle vongole: £12.50\n").to_stdout
+    # end
+
+    it 'print a list of dishes with prices' do
       menu = Menu.new
-      expect { menu.see_menu }.to output("spaghetti alle vongole: £12.50\n").to_stdout
-      # expect { print('foo') }.to output('foo').to_stdout
+      list = "spaghetti alle vongole: £12.50\nspaghetti alla carbonara: £14.40
+parmigiana: £11.30\nlasagna al forno: £14.60
+ragu alla bolognese: £12.00\nrisotto allo zafferano: £15.20
+gnocchi alla sorrentina: £9.50\nzucchine ripiene alla ligure: £14.70\n"
+      expect { menu.see_menu }.to output(list).to_stdout
     end
   end
 end
