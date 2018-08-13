d94432bec970bcff2155c3906d98f8843b7113c9
diego fontecilla <mail@diegofontecilla.com>
2018-08-13 18:15:00 +0100

    user can select several hashes with dishes and prices from the menu

--- spec/cart_spec.rb
+++ spec/cart_spec.rb
@@ -8,12 +8,19 @@ describe Cart do
 
   context '#select dish' do
 
-    it 'user can select one dish from the menu' do
+    it 'user can select a hash with dish and price from the menu' do
       cart = Cart.new
       cart.select_dish('spaghetti alle vongole')
       expect(cart.selected_dishes).not_to be_empty
     end
 
+    it 'user can select several hashes with dishes and prices from the menu' do
+      cart = Cart.new
+      cart.select_dish('spaghetti alle vongole')
+      cart.select_dish('zucchine ripiene alla ligure')
+      expect(cart.selected_dishes).to include({dish: 'zucchine ripiene alla ligure', price: '14.70'}
+)
+    end
     # expect(cart.selected_dishes).to include('spaghetti alle vongole')
   end
 end
