b678992e7c19442d1712cbd1bd9537e16a442ea6
diego fontecilla <mail@diegofontecilla.com>
2018-08-14 18:48:33 +0100

    after selecting, user can see a selection of two dish, its prices and the total

--- lib/cart.rb
+++ lib/cart.rb
@@ -14,4 +14,19 @@ class Cart
       @selected_dishes << choosen_dish if choosen_dish[:dish] == new_dish
     end
   end
+
+  def your_selection
+    add = 0
+    @selected_dishes.each do |selection|
+      puts "#{selection[:dish]}: £#{selection[:price]}, total £#{add += selection[:price].to_f}0"
+    end
+  end
+
+  def give_total
+    add = 0
+    @selected_dishes.each do |selection|
+      add += selection[:price].to_f
+    end
+    "£#{add.to_s}0"
+  end
 end
--- spec/cart_spec.rb
+++ spec/cart_spec.rb
@@ -4,23 +4,43 @@ require 'menu'
 describe Cart do
 
   let(:fake_menu) { double(:menu) }
+  subject(:cart) { described_class.new }
   # let(:fake_new_dish) { double(:new_dish)}
 
   context '#select dish' do
 
     it 'user can select a hash with dish and price from the menu' do
-      cart = Cart.new
       cart.select_dish('spaghetti alle vongole')
       expect(cart.selected_dishes).not_to be_empty
     end
 
     it 'user can select several hashes with dishes and prices from the menu' do
-      cart = Cart.new
       cart.select_dish('spaghetti alle vongole')
       cart.select_dish('zucchine ripiene alla ligure')
-      expect(cart.selected_dishes).to include({dish: 'zucchine ripiene alla ligure', price: '14.70'}
-)
+      expect(cart.selected_dishes).to include({dish: 'zucchine ripiene alla ligure', price: '14.70'})
     end
-    # expect(cart.selected_dishes).to include('spaghetti alle vongole')
+  end
+
+  context '#give_total' do
+
+    it 'return the sum of the prices of two dishes' do
+      cart.select_dish('spaghetti alle vongole')
+      cart.select_dish('zucchine ripiene alla ligure')
+      expect(cart.give_total).to eq('£27.20')
+    end
+  end
+
+  context '#your_selection' do
+
+    it  'user can see a selection of one dish, its price and the total' do
+      cart.select_dish('spaghetti alle vongole')
+      expect { cart.your_selection }.to output("spaghetti alle vongole: £12.50, total £12.50\n").to_stdout
+    end
+
+    it  'user can see a selection of two dish, its price and the total' do
+      cart.select_dish('spaghetti alle vongole')
+      cart.select_dish('spaghetti alla carbonara')
+      expect { cart.your_selection }.to output("spaghetti alle vongole: £12.50, total £12.50\nspaghetti alla carbonara: £14.40, total £26.90\n").to_stdout
+    end
   end
 end
